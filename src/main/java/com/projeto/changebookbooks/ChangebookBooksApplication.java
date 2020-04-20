0fed1c78236eb30fa9a0943beffff53ddcf3f891
GRMCosta <rafa_gian2011@hotmail.com>
2020-04-20 18:24:41 -0300

    first commit

---
+++ src/main/java/com/projeto/changebookbooks/ChangebookBooksApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.changebookbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangebookBooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChangebookBooksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projeto/changebookbooks/ChangebookBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.changebookbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangebookBooksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
