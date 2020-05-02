5dc8f236bfb2fbed333624c028cf08836f8e1d1f
Luiz Henrique <luizhds21@gmail.com>
2020-05-02 18:44:04 -0300

    Incluindo Feign Client

--- src/main/java/com/projeto/changebookbooks/ChangebookBooksApplication.java
+++ src/main/java/com/projeto/changebookbooks/ChangebookBooksApplication.java
@@ -2,7 +2,9 @@ package com.projeto.changebookbooks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class ChangebookBooksApplication {
 
--- src/main/java/com/projeto/changebookbooks/integration/user/client/UserClient.java
+++ src/main/java/com/projeto/changebookbooks/integration/user/client/UserClient.java
@@ -1,5 +1,18 @@
 package com.projeto.changebookbooks.integration.user.client;
 
+import com.projeto.changebookbooks.integration.user.response.UserResponse;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(url = "${user.url}")
 public interface UserClient {
-    //TODO
+
+    @RequestMapping(method = RequestMethod.GET, value ="/token/valid")
+    ResponseEntity validateToken(@RequestHeader("Authorization") String token);
+
+    @RequestMapping(method = RequestMethod.GET, value ="/user/by_token")
+    UserResponse getUserByToken(@RequestHeader("Authorization") String token);
 }
